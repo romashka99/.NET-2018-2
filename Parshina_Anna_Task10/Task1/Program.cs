6af96bbe802d4bec44d6c7b58a0ebba407844d20
Anna <parshina.annushka@mail.ru>
2018-11-09 02:41:19 +0300

    change task1

--- Parshina_Anna_Task10/Task1/Program.cs
+++ Parshina_Anna_Task10/Task1/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathLibrary;
 
-namespace Task2
+namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Function.Factorial(5));
+            Console.WriteLine(Function.Exponentiation(5,2));
+            Console.WriteLine(Function.Exponentiation(5.1, 2));
         }
     }
 }
